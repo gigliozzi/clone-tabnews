1150ea973596310090f2b7ba01a94220535f7297
William Gigliozzi <gigliozzi@gmail.com>
2023-12-16 01:36:05 +0000

    adiciona os scripts lint:check e lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Você é uma ótima pessoa. Se curtiu, dá uma risadinha 🤓</h1>
+function Home() {
+  return <h1>Você é uma ótima pessoa. Se curtiu, dá uma risadinha 🤓</h1>;
+}
+
+function teste2() {
+  console.log("teste");
 }
 
 export default Home;
