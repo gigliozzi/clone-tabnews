052452ff0a1c534fad8692ece4fede952481e29a
William Gigliozzi <gigliozzi@gmail.com>
2024-01-03 14:23:36 +0000

    muda a imagem da

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <div>
       <h1>Que bom que voce veio ver !!</h1>
-      <img src="/img/amigobeto.gif" alt="imagem doguinho" />
+      <img src="/img/doguinho.gif" alt="imagem doguinho" />
     </div>
   );
 }
