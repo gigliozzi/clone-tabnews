7d5161cd3c4a9e895eecad8a86aa8bc5ebaaf4ab
William Gigliozzi <gigliozzi@gmail.com>
2024-01-17 01:48:29 +0000

    cria a pasta `models` e adiciona um teste real

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,3 +1,12 @@
-test("Eu espero que seja 1", () => {
-  expect("1").toBe("1");
+const calculadora = require("../models/calculadora");
+
+test("somar 2 + 2 deveria retonar 4", () => {
+  const resultado = calculadora.somar(2, 2);
+  expect(resultado).toBe(4);
+});
+
+test("somar 2 + 100 deveria retornar 102", () => {
+  const resultado = calculadora.somar(2, 100);
+  console.log(resultado);
+  expect(resultado).toBe(102);
 });
