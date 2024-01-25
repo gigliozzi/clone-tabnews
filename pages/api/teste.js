08332543646a80942ec04138b024cf254d6279f0
William Gigliozzi <gigliozzi@gmail.com>
2024-01-25 01:23:30 +0000

    altera a `API/teste`

--- pages/api/teste.js
+++ pages/api/teste.js
@@ -1,14 +1,17 @@
 function teste(request, response) {
-  response.status(200).json({
-    cargo1: "BUSINESS PARTNER",
-    cargo2: "CURADOR LIDER",
-    cargo3: "DIRETOR*",
-    cargo4: "GERENTE*",
-    cargo5: "GESTOR*",
-    cargo6: "PRÓ-REITOR*",
-    cargo7: "VICE DIRETOR*",
-    cargo8: "VICE PRESIDENTE*",
-    cargo9: "VICE REITOR*"
-  });
+  response.status(200).json([
+    {
+      sgl: "AT",
+      behavior: "ignore"
+    },
+    {
+      sgl: "LM",
+      behavior: "reset"
+    },
+    {
+      sgl: "BE",
+      behavior: "block"
+    }
+  ]);
 }
 export default teste;
